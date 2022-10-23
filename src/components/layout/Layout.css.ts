6a32f1f14fc173fae81d37d59bd2940d0a31c4b4
Alexander Almström <info@alexanderalmstrom.com>
2022-10-24 00:23:25 +0200

    Remove main layout css

--- src/components/layout/Layout.css.ts
+++ src/components/layout/Layout.css.ts
@@ -1,9 +1,3 @@
 import { style } from "@vanilla-extract/css";
-import { container } from "../../styles/container.css";
 
-export const main = style([
-  container,
-  {
-    maxWidth: 1170,
-  },
-]);
+export const main = style({});
