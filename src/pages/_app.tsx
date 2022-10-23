0c8029cfa4156780a99a271d9c648b080c23168a
Alexander Almström <info@alexanderalmstrom.com>
2022-10-24 00:28:07 +0200

    Test setting dark as default theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
       </Head>
-      <ThemeProvider>
+      <ThemeProvider defaultTheme="dark">
         <Theme>
           <Component {...pageProps} />
         </Theme>
